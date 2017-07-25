afc32850882ab8e5a7563ce41b544a46de2b4ef4
Andi Neck <andreas.neck.ext@siemens.com>
2017-07-25 16:57:19 +0200

    add boards

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/local/bin/node
-
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
@@ -11,14 +10,21 @@ let args = require('subarg')(process.argv.slice(2));
 let command = args._[0];
 let cmdArgs = args._.join(' ');
 
-const FILENAME = `${process.cwd()}/.git-lab`;
+const SETTINGS_HOME = `${require('os').homedir()}/.git-lab`;
+const SETTINGS_LOCAL = `${process.cwd()}/.git-lab`;
+const API_VERSION = 'v4';
 
+let debug = process.env.DEBUG || args.d || args.debug;
 let settings = {};
 try {
-  settings = JSON.parse(fs.readFileSync(FILENAME, 'utf8'));
+  settings = JSON.parse(fs.readFileSync(SETTINGS_LOCAL, 'utf8'));
 } catch (e) {
-  console.error('no settings file found (.git-lab)');
+  if (debug) console.error('no settings file found ".git-lab"');
 }
+
+let token = process.env.TOKEN || args.t || args.token || args.access_token || args['access-token'] || settings.token;
+let url = process.env.URL || args.u || args.url || settings.url;
+let group = process.env.GROUP || args.g || args.group || settings.group;
 
 if (args.h || args.help) {
   console.log('\ngitlab tool\n');
@@ -34,13 +40,9 @@ if (args.h || args.help) {
   process.exit();
 }
 
-let token = process.env.TOKEN || args.t || args.token || args.access_token || args['access-token'] || settings.token;
-let url = process.env.URL || args.u || args.url || settings.url;
-let group = process.env.GROUP || args.g || args.group || settings.group;
-let debug = process.env.DEBUG || args.d || args.debug || settings.debug;
-
 if (debug) {
-  console.log('settings filename: ', process.cwd(), FILENAME);
+  console.log('settings file local: ', SETTINGS_LOCAL);
+  console.log('settings file home: ', SETTINGS_HOME);
   console.log('settings', settings);
   console.log(`TOKEN ${token}`);
   console.log(`URL ${url}`);
@@ -50,29 +52,56 @@ if (debug) {
   console.log('command with arguments: ', cmdArgs);
 }
 
-
-if (!token || !url || !group) {
+if (!token || !url) {
   console.error('must provide TOKEN, URL and GROUP as environment variables');
   console.error('or provide them as arguments:  --token --url --group');
   process.exit(-1)
 }
 
 if (args.s || args.save) {
-  fs.writeFileSync(FILENAME, JSON.stringify({ token, url, group }, null, 2), 'utf8');
+  fs.writeFileSync(SETTINGS_LOCAL, JSON.stringify({ token, url, group }, null, 2), 'utf8');
 }
 
-fetch(`${url}/api/v3/groups/${group}/projects\?private_token\=${token}&per_page=999`)
-  .then(function (response) {
-    if (response.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return response.json();
-  })
-  .then(function (projects) {
-    projects.forEach(project => {
-      execute(project.http_url_to_repo, command, cmdArgs);
+if (command === 'get-boards') {
+  process.stdin.pipe(require('split')()).on('data', processLine)
+  function processLine(projectId) {
+    var projectId = projectId.replace(/\//g, '%2F')
+    if (!projectId.trim()) return;
+    console.log('projectId' + projectId)
+    fetch(`${url}/api/${API_VERSION}/projects/${projectId}/boards?private_token\=${token}`)
+      .then(handleError)
+      .then((boards) => {
+        boards.forEach((board) => console.log(board));
+      })
+      //.catch(reason => console.log("catch" + reason))
+  }
+} else if (command === 'add-label') {
+  process.stdin.pipe(require('split')()).on('data', processLine)
+  function processLine(line) {
+    console.log('bla' + line + '!')
+  }
+} else if (command === 'ls') {
+  fetch(`${url}/api/${API_VERSION}/projects?private_token\=${token}&per_page=999`)
+    .then(handleError)
+    .then((projects) => {
+      projects.forEach((project) => console.log(project.path_with_namespace));
     })
-  });
+} else {
+  fetch(`${url}/api/${API_VERSION}/groups/${group}/projects\?private_token\=${token}&per_page=999`)
+    .then(handleError)
+    .then(function (projects) {
+      projects.forEach(project => {
+        execute(project.http_url_to_repo, command, cmdArgs);
+      })
+    });
+}
+
+function handleError(res) {
+  if (res.status >= 400) {
+    throw new Error("Bad response from server");
+  }
+  return res.json();
+}
 
 function execute(repo, command, cmdArgs) {
   if (command === 'clone') {
